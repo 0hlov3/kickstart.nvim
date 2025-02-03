f2e0a70bae2ecae8e76021b04c2a88f6358eb557
0hlov3 <dev@schoenwald.aero>
2025-02-04 00:26:00 +0100

    Adds some Language Servers

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -616,7 +616,45 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        ansiblels = {
+          ansible = {
+            ansible = {
+              path = 'ansible',
+            },
+            executionEnvironment = {
+              enabled = false,
+            },
+            python = {
+              interpreterPath = 'python',
+            },
+            validation = {
+              enabled = true,
+              lint = {
+                enabled = true,
+                path = 'ansible-lint',
+              },
+            },
+          },
+        },
+
+        bashls = {
+          bashIde = {
+            globPattern = '*@(.sh|.inc|.bash|.command)',
+          },
+        },
         -- clangd = {},
+        docker_compose_language_service = {},
+        dockerls = {
+          settings = {
+            docker = {
+              languageserver = {
+                formatter = {
+                  ignoreMultilineInstructions = true,
+                },
+              },
+            },
+          },
+        },
         -- gopls = {}
         gopls = {
           gopls = {
@@ -628,6 +666,10 @@ require('lazy').setup({
             staticcheck = true,
           },
         },
+        helm_ls = {},
+        templ = {},
+        terraformls = {},
+        tflint = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -638,7 +680,15 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
-
+        yamlls = {
+          settings = {
+            redhat = {
+              telemetry = {
+                enabled = false,
+              },
+            },
+          },
+        },
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
