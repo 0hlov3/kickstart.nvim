6f573c3922ed5e47f2650a17806598037dde252b
0hlov3 <dev@schoenwald.aero>
2025-02-07 16:49:17 +0100

    Configures spell checking

--- init.lua
+++ init.lua
@@ -98,6 +98,9 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- spell checking
+vim.opt.spell = true
+vim.opt.spelllang = { 'en_us', 'de_de' }
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
